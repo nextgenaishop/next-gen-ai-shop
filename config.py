818b0485b2ea7832b92eb2f149f0c1cbcc7a6504
nextgenaishop <aminuloctober2021@gmail.com>
2026-06-28 11:21:53 +0800

    Fix formatting issues in config.py
    Add config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+
+import os
+STORE_NAME = "Next Gen AI Shop"
+SUPPORT = "@digitalproduct786"
+BINANCE_PAY_ID = "486825563"
+BKASH = "01619431937"
+NAGAD = "01619431937"
+GITHUB_IMAGE_BASE = "https://raw.githubusercontent.com/nextgenaishop/next-gen-ai-shop/main/"
